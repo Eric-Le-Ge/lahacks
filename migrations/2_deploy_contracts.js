ec33d3b13c952322e35ad1ab731816457544781b
Eric-Le-Ge <ericlege@berkeley.edu>
2018-03-31 16:50:32 -0700

    testing'

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 var DogeFlow = artifacts.require("DogeFlow");
-var DogeContract = artifacts.require("DogeContract");
+
 
 
 module.exports = function(deployer) {
   deployer.deploy(DogeFlow);
-  deployer.deploy(DogeContract);
+  
 };
